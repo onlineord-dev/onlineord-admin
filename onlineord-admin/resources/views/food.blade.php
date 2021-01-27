5044f09376b20cef0911f835c576542b0542f786
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-01-27 23:21:59 +0200

    ❌ Remove project

--- onlineord-admin/resources/views/food.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-
-@section('title')
-    OnlineOrd Admin
-@endsection
-
-@section('content')
-    <h1>Cписок їжі</h1>
-@endsection
--- onlineord-admin/resources/views/menu.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-
-@section('title')
-    OnlineOrd Admin
-@endsection
-
-@section('content')
-    <h1>Меню</h1>
-@endsection
