810f4890b24f187c60d1df9a332fe4acb3c6b73c
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-01-28 01:11:19 +0200

    Add css

--- admin/resources/views/layout.blade.php
+++ admin/resources/views/layout.blade.php
@@ -6,21 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link href="./resources/css/app.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="./resources/css/app.css" rel="stylesheet">
-{{--    <style>--}}
-
-{{--        body {--}}
-{{--            background-color: aquamarine;--}}
-{{--            margin: 0;--}}
-{{--            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',--}}
-{{--            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',--}}
-{{--            sans-serif;--}}
-{{--            -webkit-font-smoothing: antialiased;--}}
-{{--            -moz-osx-font-smoothing: grayscale;--}}
-{{--        }--}}
-{{--    </style>--}}
+    <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="d-flex" id="wrapper">
