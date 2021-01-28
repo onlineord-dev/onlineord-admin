40b061bbeaf6104c6e603fc4560de10b1666c53f
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-01-28 18:56:56 +0200

    Connect to database

--- admin/resources/views/food.blade.php
+++ admin/resources/views/food.blade.php
@@ -6,4 +6,11 @@
 
 @section('content')
     <h1>Cписок страв</h1>
+    <?php
+        if(DB::connection()->getPdo())
+        {
+            echo "Successfully connected to the database => "
+                .DB::connection()->getDatabaseName();
+        }
+    ?>
 @endsection
