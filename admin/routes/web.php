5e33c8fc521e75f7f01a34044e1651ee77146b08
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-01-28 15:32:44 +0200

    Sidebar menu

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
+});
+
+Route::get('/admin', function () {
+    return view('home');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/food', function () {
+    return view('food');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
