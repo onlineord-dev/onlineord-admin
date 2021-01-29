fc2b88240932645c0ce98af595e97713b9000da9
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-01-29 20:45:59 +0200

    test

---
+++ onlineord-admin/storage/framework/views/2720e09bc1c88a0e2e4a88180ca0b385a9d088e4.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection("home"); ?>
+    <h1>Home</h1>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make("layout", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\vital\Documents\GitHub\onlineord-admin\onlineord-admin\resources\views/home.blade.php ENDPATH**/ ?>
---
+++ onlineord-admin/storage/framework/views/4b99870bce600f09da7eaf95a3c69d0906db4456.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OnlineOrders Admin
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+<?php echo $__env->yieldContent('home'); ?>
+</body>
+</html>
+<?php /**PATH C:\Users\vital\Documents\GitHub\onlineord-admin\onlineord-admin\resources\views/layout.blade.php ENDPATH**/ ?>
