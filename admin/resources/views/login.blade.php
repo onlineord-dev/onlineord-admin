88d40884591239ce2963fe4eceddb7cbe0ed1621
Ihor Klymenko <ihor.klymenko1@gmail.com>
2021-04-01 18:05:12 +0300

    Login start commit

--- admin/resources/views/login.blade.php
+++ admin/resources/views/login.blade.php
@@ -4,19 +4,17 @@
     OnlineOrd Admin
 @endsection
 
-@section('header')
-    Вхід
-@endsection
-
 @section('fields')
-    <input type="email" id="inputEmail" class="form-control bottom-corners-0" placeholder="Ел. пошта">
-    <input type="password" id="inputPassword" class="form-control top-corners-0" placeholder="Пароль">
+    <form method="POST" action="{{route('login')}}">
+        <img class="mb-4" src="img/logo.png" alt="" width="72">
+        <h1 class="h3 mb-3 fw-normal">Вхід</h1>
+
+        <input type="email" id="inputEmail" class="form-control bottom-corners-0" placeholder="Ел. пошта" value="">
+        <input type="password" id="inputPassword" class="form-control top-corners-0" placeholder="Пароль" valut="">
+
+        <button type="submit" class="w-100 btn btn-lg btn-main mt-3">Ввійти</button>
+        <p class="mt-5 mb-3 text-muted">Не маєте акаунту? <a href="/registration">Створити</a></p>
+    </form>
 @endsection
 
-@section('button-label')
-    Ввійти
-@endsection
 
-@section('link')
-    Не маєте акаунту? <a href="/registration">Створити</a>
-@endsection
--- admin/routes/web.php
+++ admin/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
@@ -9,10 +10,21 @@ use App\Http\Controllers\FoodController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegistrationController;
 
-Route::get('/', [LoginController::class, 'login']);
-Route::get('/admin', [HomeController::class, 'home']);
+//Route::get('/', [LoginController::class, 'login']);
+//Route::get('/admin', [HomeController::class, 'home']);
 Route::get('/profile', [ProfileController::class, 'profile']);
 Route::get('/menu', [MenuController::class, 'menu']);
 Route::get('/food', [FoodController::class, 'food']);
 Route::get('/login', [LoginController::class, 'login']);
 Route::get('/registration', [RegistrationController::class, 'registration']);
+
+
+Route::view('/admin','home')->middleware('auth')->name('home');
+Route::get('/',function(){
+    if(Auth::check()){
+        return redirect(route('home'));
+    }
+    return view('login');
+})->name('login');
+//Route::post('/login',[]);
+//Route::get('/logout',[])->name('logout');
